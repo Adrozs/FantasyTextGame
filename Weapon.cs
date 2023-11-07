204fb5cf9ccea358fba74e5fff7b4d4f8a6f3435
Adrozs <adrozsahegyi@gmail.com>
2023-11-07 16:02:09 +0100

    Removed sword class and replaced with weapon class. To make it easier later when adding Sorcerer and Shadow's weapons

--- Weapon.cs
+++ Weapon.cs
@@ -6,18 +6,21 @@ using System.Threading.Tasks;
 
 namespace FantasyConsoleGame
 {
-    public class Sword
+    public class Weapon
     {
         // Declare properties
         public string Name { get; set; }
         public int WeaponDmg { get; set; }
+        public int Price { get; set; }
+        public string Type { get; set; }
 
         // When creating object, take input and use it to set the values
-        public Sword(string name, int dmg)
+        public Weapon(string name, string type, int dmg, int price)
         {
             Name = name;
             WeaponDmg = dmg;
+            Price = price;
+            Type = type;
         }
-
     }
 }
